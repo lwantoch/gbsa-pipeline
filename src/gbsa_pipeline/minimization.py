be41ce26e1d5ecc1a70f74dd512af946ea5f0111
lwantoch <l.wantoch@icloud.com>
2026-02-09 15:36:24 +0100

    corrects the minimisation steps

--- src/gbsa_pipeline/minimization.py
+++ src/gbsa_pipeline/minimization.py
@@ -3,34 +3,21 @@
 from __future__ import annotations
 
 import BioSimSpace as BSS
+import sire
 
 
-def preparing_protocol(nsteps: int):
-    """Create a minimisation protocol."""
-    return BSS.Protocol.Minimisation(steps=nsteps)
+def run_minimization(nsteps: int, system: sire.System, engine: str = "GROMACS"):
 
-
-def setting_minimization(protocol, solvated: BSS._SireWrappers.System):
-    """Run minimisation and return the minimised system."""
-    min_process = BSS.MD.run(
-        solvated,
+    protocol = BSS.Protocol.Minimisation(steps=nsteps)
+    process = BSS.MD.run(
+        system,
         protocol,
-        engine="auto",
+        engine=engine,
         gpu_support=False,
         auto_start=True,
         name="min",
         property_map={},
     )
-    return min_process.getSystem(block=True)
-
-
-def run_minimization(nsteps: int, solvated: BSS._SireWrappers.System):
-
-    protocol = preparing_protocol(nsteps)
-    min_process = BSS.Process.Gromacs(protocol, solvated)
-
-    min_process.start()
-    min_process.wait()
-    minimized = min_process.getSystem()
+    minimized = process.getSystem(block=True)
 
     return minimized
